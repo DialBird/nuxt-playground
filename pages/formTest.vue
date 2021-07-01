252733a30870b188bd3df9b408f2f2b9f3accaf7
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-01 10:18:51 +0900

    [master] add name field

---
+++ pages/formTest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pt-12 px-12">
+    <p class="text-3xl font-bold mb-4">Form Sample</p>
+    <form action="post" @submit.prevent="onSubmit">
+      <fieldset class="flex flex-col gap-2 w-96 mb-4">
+        <label>name</label>
+        <input
+          v-model="form.name"
+          type="text"
+          class="rounded border-gray-500"
+        />
+      </fieldset>
+      <button
+        type="submit"
+        class="px-4 py-2 bg-black hover:opacity-95 text-white rounded font-bold"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'FormTest',
+  setup() {
+    const form = reactive({
+      name: '',
+    })
+
+    const onSubmit = () => {
+      console.log('fo', form)
+    }
+
+    return { form, onSubmit }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>

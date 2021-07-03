16fe64cd28a74280a05490849636ff6b6881cd93
Keisuke Taniguchi <rememberthatk@icloud.com>
2021-07-04 08:35:58 +0900

    [master] add useTestVue

---
+++ src/components/MouseChecker.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="target" class="o-mouse-checker">
+    <p>mousechecker: {{ `${x} : ${y}` }}</p>
+    <p>inside mousechecker: {{ `${x2} : ${y2} : ${isOutside}` }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { useMouse, useMouseInElement } from '@vueuse/core'
+
+export default defineComponent({
+  name: 'MouseChecker',
+  setup() {
+    const target = ref(null)
+    const { x, y } = useMouse()
+    const { elementX: x2, elementY: y2, isOutside } = useMouseInElement(target)
+
+    return { x, y, x2, y2, isOutside, target }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.o-mouse-checker {
+  border: 1px solid #333;
+  width: 300px;
+  height: 300px;
+}
+</style>
---
+++ src/pages/useVueTest.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container mx-auto mt-8">
+    <p class="text-2xl font-bold mb-4">useVue</p>
+    <p>{{ `mouse is ${x} : ${y}` }}</p>
+    <p>dark: {{ isDark }}</p>
+    <p>local: {{ store }}</p>
+    <MouseChecker />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useMouse, usePreferredDark, useLocalStorage } from '@vueuse/core'
+import MouseChecker from '@/components/MouseChecker.vue'
+
+export default defineComponent({
+  name: 'UseVueTest',
+  components: {
+    MouseChecker,
+  },
+  setup() {
+    const { x, y } = useMouse()
+
+    // is user prefers dark theme
+    const isDark = usePreferredDark()
+
+    // persist state in localStorage
+    const store = useLocalStorage('my-storage', {
+      name: 'Apple',
+      color: 'red',
+    })
+
+    return { x, y, isDark, store }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
